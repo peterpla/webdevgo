8a153436cd81bb1900a3afdee85d9c3298484566
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-04-27 15:14:15 -0700

    add custom 404 handler, working

--- main.go
+++ main.go
@@ -23,9 +23,21 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>FAQ</h1><p>This is my awesome FAQ page!</p>")
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h2>404 Not Found</h2><p>Sorry, we could not find "+r.RequestURI+"</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(NotFound)
 	http.ListenAndServe(":3000", r)
 }
