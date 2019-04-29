5c011cd9dcc79421517f4d64311d5cb4f9d32e29
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-04-29 15:21:04 -0700

    completed 6.5.3, working

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"./views"
 
@@ -15,38 +13,17 @@ var homeView *views.View
 var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
-	// log.Println("entered home()")
-	// log.Printf("homeView: %T, %+v", homeView, *homeView)
-	// log.Printf("homeView.Template: %T, %+v", homeView.Template, *homeView.Template)
-
 	w.Header().Set("Content-Type", "text/html")
-
-	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
-	if err != nil {
-		log.Printf("homeView.Execute() returned error: %v", err)
-		os.Exit(1)
+	if err := homeView.Render(w, nil); err != nil {
+		panic(err)
 	}
-	// log.Println("exiting home()")
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
-	// log.Println("entered contact()")
-	// log.Printf("contactView: %T, %+v", contactView, *contactView)
-	// log.Printf("contactView.Template: %T, %+v", contactView.Template, *contactView.Template)
-
 	w.Header().Set("Content-Type", "text/html")
-
-	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
-	if err != nil {
-		log.Printf("contactView.Execute() returned error: %v", err)
-		os.Exit(1)
+	if err := contactView.Render(w, nil); err != nil {
+		panic(err)
 	}
-	// log.Println("exiting contact()")
-}
-
-func Faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>FAQ</h1><p>This is my awesome FAQ page!</p>")
 }
 
 func NotFound(w http.ResponseWriter, r *http.Request) {
@@ -57,17 +34,11 @@ func NotFound(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	// log.Printf("homeView: %T, %+v", homeView, *homeView)
-	// log.Printf("homeView.Template: %T, %+v", homeView.Template, *homeView.Template)
-
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	// log.Printf("contactView: %T, %+v", contactView, *contactView)
-	// log.Printf("contactView.Template: %T, %+v", contactView.Template, *contactView.Template)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", Faq)
 	r.NotFoundHandler = http.HandlerFunc(NotFound)
 
 	http.ListenAndServe(":3000", r)
