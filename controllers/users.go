f98dbe7218e9a20ef623a79dce163c8494f11d62
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-05-01 20:03:22 -0700

    completed 7.4.3, working

--- controllers/users.go
+++ controllers/users.go
@@ -32,5 +32,9 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create: This is a temporary response")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 }
