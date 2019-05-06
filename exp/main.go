7abf4f544ddd972392790d3bd6f55860f918e06b
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-05-06 13:57:57 -0700

    completed 8.4.8

--- exp/main.go
+++ exp/main.go
@@ -21,6 +21,13 @@ type User struct { // database table "users"
 	Email string `gorm:"not null;unique_index"`
 }
 
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
+}
+
 func main() {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
@@ -32,16 +39,39 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+
 	db.LogMode(true)
+	db.AutoMigrate(&User{}, &Order{})
 
 	// postgresql://[user[:password]@][netloc][:port][/dbname]
 	fmt.Printf("Successfully connected! postgresql://%s:\"%s\"@%s:%d/%s\n", user, "", host, port, dbname)
 
-	var users []User
-	db.Find(&users)
+	var user User
+	db.First(&user)
 	if db.Error != nil {
 		panic(db.Error)
 	}
-	fmt.Println("Retrieved", len(users), "users.")
-	fmt.Println(users)
+
+	createOrder(db, user, 1001, "Fake Description #1")
+	createOrder(db, user, 9999, "Fake Description #2")
+	createOrder(db, user, 8800, "Fake Description #3")
+
+	// var users []User
+	// db.Find(&users)
+	// if db.Error != nil {
+	// 	panic(db.Error)
+	// }
+	// fmt.Println("Retrieved", len(users), "users.")
+	// fmt.Println(users)
+}
+
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
+	}
 }
