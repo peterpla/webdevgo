24c64b35c4a52f947b50fe18dc9b48e5f6650ea5
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-05-05 17:47:38 -0700

    completed 8.3.5, working

--- exp/main.go
+++ exp/main.go
@@ -30,5 +30,52 @@ func main() {
 	}
 
 	fmt.Println("Successfully connected!")
+
+	// var name = "Jon Calhoun"
+	// var email = "jon@calhoun.io"
+
+	// _, err = db.Exec(`
+	//   INSERT INTO users(name, email)
+	//   VALUES ($1, $2)`,
+	// 	name, email)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Successfully inserted %s, %s!\n", name, email)
+
+	// name = "Jon2 Calhoun2"
+	// email = "jon2@calhoun2.io"
+
+	// row := db.QueryRow(`
+	//   INSERT INTO users(name, email)
+	//   VALUES($1, $2) RETURNING id`,
+	// 	name, email)
+
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Successfully inserted %s, %s!\n", name, email)
+
+	var id int
+	var name, email string
+
+	rows, err := db.Query(`
+  	  SELECT id, name, email
+  	  FROM users
+		WHERE email = $1
+		OR ID > $2`,
+		"jon@calhoun.io", 3)
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		rows.Scan(&id, &name, &email)
+		fmt.Println("ID:", id, "Name:", name, "Email:", email)
+	}
+
 	db.Close()
 }
